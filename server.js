cf5cb65cc29ba389820e0a04f13c296ce51c2038
Tahir KOCAKURT <tkocakurt@gmail.com>
2023-05-12 16:48:53 +0300

    addes app.js file

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
 const mongoose = require("mongoose");
-const express = require("express");
 
-const app = express();
-
+const app = require("./app");
 const port = process.env.PORT || 3000;
+const uri = process.env.DATABASE.replace(
+  /<PASSWORD>/g,
+  process.env.DATABASE_PASSWORD
+);
+mongoose.connect(uri).then(() => {
+  console.log("Connection established with DB Server");
+});
 
-app.listen(port);
+app.listen(port, () => console.log(`Listening on ${port} ...`));
